ed9d371aa07d19dcb949dba5178176d48d799037
Antonio Salazar <salazar3antonio@gmail.com>
2016-08-01 17:35:06 -0500

    Added new Word Class
    this class will store my model data

---
+++ app/src/main/java/co/wordywordy/wordywordy/data/Word.java
@@ -0,0 +1,15 @@
+package co.wordywordy.wordywordy.data;
+
+/**
+ * Created by Desktop on 8/1/2016.
+ */
+public class Word {
+
+    private String definition;
+    private String word;
+    private String synonym;
+
+    public Word(String word) {
+        this.word = word;
+    }
+}
