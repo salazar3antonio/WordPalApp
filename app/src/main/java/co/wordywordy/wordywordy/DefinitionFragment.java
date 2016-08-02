bc2268a57a42b2f977d658c0f92d715c3a91a516
Antonio Salazar <salazar3antonio@gmail.com>
2016-08-01 20:43:13 -0500

    Added New Fragment Class

---
+++ app/src/main/java/co/wordywordy/wordywordy/DefinitionFragment.java
@@ -0,0 +1,12 @@
+package co.wordywordy.wordywordy;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Desktop on 8/1/2016.
+ */
+public class DefinitionFragment extends Fragment {
+
+    
+
+}
--- app/src/main/java/co/wordywordy/wordywordy/data/Word.java
+++ app/src/main/java/co/wordywordy/wordywordy/data/Word.java
@@ -6,10 +6,35 @@ package co.wordywordy.wordywordy.data;
 public class Word {
 
     private String definition;
-    private String word;
+    private String main_word;
     private String synonym;
 
-    public Word(String word) {
-        this.word = word;
+    public Word(String main_word) {
+        this.main_word = main_word;
+    }
+
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getMain_word() {
+        return main_word;
+    }
+
+    public void setMain_word(String main_word) {
+        this.main_word = main_word;
+    }
+
+    public String getSynonym() {
+        return synonym;
+    }
+
+    public void setSynonym(String synonym) {
+        this.synonym = synonym;
     }
 }
