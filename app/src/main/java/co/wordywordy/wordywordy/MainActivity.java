63698e645b3228526d953e71754bc650f2914494
Antonio Salazar <salazar3antonio@gmail.com>
2016-08-03 20:52:57 -0500

    Cleaned up TakeQuiz

--- app/src/main/java/co/wordywordy/wordywordy/MainActivity.java
+++ app/src/main/java/co/wordywordy/wordywordy/MainActivity.java
@@ -1,14 +1,20 @@
 package co.wordywordy.wordywordy;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements QuizListFragment.OnFragmentInteractionListener {
 
     private Spinner areaSpinner;
     private Spinner levelSpinner;
+    private Button takeQuizButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
         areaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         levelSpinner.setAdapter(levelAdapter);
 
+        takeQuizButton = (Button) findViewById(R.id.take_quiz_button);
+        takeQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(QuizListFragment.newInstance("test", "test2"), "TAG");
+                fragmentTransaction.commit();
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
