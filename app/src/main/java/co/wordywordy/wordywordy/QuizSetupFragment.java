e83c08629a841dfe0080fcd6de5f282e5adc284b
Antonio Salazar <salazar3antonio@gmail.com>
2016-08-06 09:25:11 -0500

    Wired up RadioGroups

--- app/src/main/java/co/wordywordy/wordywordy/QuizSetupFragment.java
+++ app/src/main/java/co/wordywordy/wordywordy/QuizSetupFragment.java
@@ -5,18 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.RadioGroup;
 
 import co.wordywordy.wordywordy.data.QuizList;
 
 
 public class QuizSetupFragment extends Fragment {
 
-    private Spinner areaSpinner;
-    private Spinner levelSpinner;
-    private Button takeQuizButton;
+    private RadioGroup mAreaRadioGroup;
+    private RadioGroup mLevelRadioGroup;
+    private Button mTakeQuizButton;
     private QuizList mQuizList;
 
     public QuizSetupFragment() {
@@ -41,8 +40,57 @@ public class QuizSetupFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_quiz_setup, container, false);
         mQuizList = new QuizList();
 
-        takeQuizButton = (Button) view.findViewById(R.id.take_quiz_button);
-        takeQuizButton.setOnClickListener(new View.OnClickListener() {
+        mAreaRadioGroup = (RadioGroup) view.findViewById(R.id.area_radioGroup);
+        mLevelRadioGroup = (RadioGroup) view.findViewById(R.id.level_radioGroup);
+
+        mAreaRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (mAreaRadioGroup.getCheckedRadioButtonId()) {
+                    case R.id.es_radio:
+                        mQuizList.setArea("es");
+                        break;
+                    case R.id.ms_radio:
+                        mQuizList.setArea("ms");
+                        break;
+                    case R.id.hs_radio:
+                        mQuizList.setArea("hs");
+                        break;
+                    case R.id.sat_radio:
+                        mQuizList.setArea("sat");
+                        break;
+                    case R.id.all_radio:
+                        mQuizList.setArea("oeverall");
+                        break;
+                }
+            }
+        });
+
+        mLevelRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (mLevelRadioGroup.getCheckedRadioButtonId()) {
+                    case R.id.two_radio:
+                        mQuizList.setLevel(2);
+                        break;
+                    case R.id.four_radio:
+                        mQuizList.setLevel(4);
+                        break;
+                    case R.id.six_radio:
+                        mQuizList.setLevel(6);
+                        break;
+                    case R.id.eight_radio:
+                        mQuizList.setLevel(8);
+                        break;
+                    case R.id.ten_radio:
+                        mQuizList.setLevel(10);
+                        break;
+                }
+            }
+        });
+
+        mTakeQuizButton = (Button) view.findViewById(R.id.take_quiz_button);
+        mTakeQuizButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
